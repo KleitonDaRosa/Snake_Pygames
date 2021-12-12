ff9fea8c8a0d753672fa49f56b7e8ff0c75be7d6
Kleiton Da Rosa <82580588+KleitonDaRosa@users.noreply.github.com>
2021-12-12 22:05:09 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+"""
+@author:Kleiton Da Rosa
+State:Em desenvolvimento
+Titulo : Snake_version 0.1
+"""
+import pygame
+from pygame.locals import *
+
+"""
+*Anotaçoes pessoais
+----> Precisamos Criar a tela e passar os tamanhos
+      Para isso criei uma variavel WINDOWS_SIZE = (600,600) 
+
+"""
+
+
+WINDOWS_SIZE = (600 , 600)
+PIXEL_SIZE  = 10
+
+pygame.init()
+#Criando a tela do jogo
+screen = pygame.display.set_mode(WINDOWS_SIZE)
+pygame.display.set_caption('Snake')
+
+
+#Criando o elemento Cobrinha->Tipo Lista
+snake_position = [(250,50),(260,50),(270,50)]
+snake_surface = pygame.Surface((PIXEL_SIZE,PIXEL_SIZE))
+snake_surface.fill((255,255,255))
+
+#Snake Diretion
+snake_diretion = K_LEFT
+
+#Criando a Maçacinha
+apple_surface = pygame.Surface((PIXEL_SIZE,PIXEL_SIZE))
+apple_surface.fill((255,0,0))
+
+
+
+
+while True:
+
+    pygame.time.Clock().tick(15)#Tempo de movimento
+    screen.fill((0,0,0,))
+    #Para fechar a tela
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            quit()
+
+        elif event.type == KEYDOWN:
+            if event.key in [K_UP,K_DOWN,K_LEFT,K_RIGHT]:
+                snake_diretion = event.key
+
+
+    #Exibindo a cobrinha na tela
+    for pos in snake_position:
+        screen.blit(snake_surface,pos)
+
+    #snake_position[0] = snake_position[0][0]+10, snake_position[0][1]
+
+    if snake_diretion == KEYUP:
+        snake_position[0] = (snake_position[0][0], snake_position[0][1]-10 )
+
+    pygame.display.update()
+
+
